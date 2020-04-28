5564cea7760a638770f8d4e56117093e80790671
jmhen <enna_zhou@mymail.sutd.edu.sg>
2020-04-29 00:11:01 +0800

    push contract source code

--- backend/contract.sol
+++ backend/contract.sol
@@ -1,3 +1,16 @@
+pragma solidity >=0.4.22 <0.7.0;
+
+/*  RULES:
+    1.	Only members added by administrator can vote.
+    2.	Every member is himself/herself a candidate.
+    3.	Every member’s vote will count and count for once only.
+    4.	Nobody should know who other members vote for.
+    5.	The election result will be published if
+    a.	everyone has voted or
+    b.	deadline has been reached
+    6.	The election results will showcase all candidates with non-zero votes 
+*/
+
 contract Owner {
 
     address private owner;
@@ -25,20 +38,33 @@ contract Owner {
 contract RepElection is Owner{
     
     struct Member {
-        bytes32 name; // Names will be showcased for easier voting
+        uint id; // For indexed search of name or result
         bool voted; // For checking if everyone has voted only once
-        address vote;  // The member voted for
-        uint noOfVotes; // Number of votes received 
+        uint vote;  // The member voted for
     }
     
-    
+    // public states
+    uint public deadline;
+    bytes32 public role;
     mapping (address=>bool) public access;
-    mapping(address=>Member) private members;
+    uint public memberCount;
+    uint public totalVoted;
+    bool public setupFinished;
+ 
     
-    bool membersAdded;
-    uint deadline;
+    // private states
+    mapping (address=>Member) private members;
+    bytes32[] private memberList;
+    uint[] private results;
     
+    
+    // lock
+    bool datalock;
+    
+    // event
     event LogVote(address member);
+    event LogCreation(bytes32 role, uint deadline);
+    event LogMemberAdded(bytes32 name, address account);
     
     
      /**
@@ -52,9 +78,9 @@ contract RepElection is Owner{
         _;
     }
     
-    modifier canPublish {
+    modifier resultsPublished {
         require(
-            now >= deadline, // Check if deadline has reached or all members have voted.
+            totalVoted==memberCount||now>deadline, // Check if deadline has reached or all members have voted.
             "The Election Results cannot be published yet."
         );
 
@@ -65,57 +91,84 @@ contract RepElection is Owner{
      * defaults
      */
      
-    constructor() {
-        deadline = now + 10 * 1 days;
+    constructor(uint timestamp,bytes32 rolename) public {
+        deadline = timestamp;
+        role = rolename;
+        emit LogCreation(rolename, timestamp);
     }
     
     /**
      * Operations by administrator
      */
-     
-    
-    function setDeadline(uint _days) public isOwner{
-        deadline = now + _days * 1 days;
+ 
+    function addMember(bytes32 name, address acct) public isOwner{
+        // Add member and assign index id.
+        require(!datalock);
+        datalock=true;
+        
+        access[acct] = true;
+        members[acct] = Member (memberCount,false,0);
+        memberList.push(name);
+        results.push(0);
+        memberCount += 1;
+        
+        datalock=false;
+        emit LogMemberAdded(name,acct);
     }
     
-    function addMember(bytes32 name, address acct) public isOwner{
-        access[acct] = true;
-        members[acct] = Member (name,false,0,0);
+
+    function finishSetup() public isOwner {
+        setupFinished = true;
     }
     
     function endElection() public isOwner{
         selfdestruct(msg.sender);
     }
     
+    
+    
     /**
      * Operations by members
      */
     
-    
-     
-    function vote(address target) public isMember{
+    function vote(uint target) public isMember{
         require(
            !members[msg.sender].voted, 
             "You have already voted."
         );
         members[msg.sender].voted = true;
         members[msg.sender].vote = target;
+        results[target] += 1;
+        totalVoted +=1;
         emit LogVote(msg.sender);
     }
     
+    function getName() public view isMember returns(bytes32) {
+        uint myID = members[msg.sender].id;
+        return memberList[myID];
+    }
+    
+    function getVote() public view isMember returns(uint) {
+        require(members[msg.sender].voted);
+        return members[msg.sender].vote;
+    }
+    
+    
     
     /**
-     * General methods for frontend
+     * General getters 
      */ 
      
-    function getName ( address addr) public returns (bytes32){
-        return members[addr].name;
+    function getMemberList() public view returns(bytes32[]) {
+        return memberList;
     }
     
-    function publish () public canPublish{
-        //sorting of results
-    }
     
+    function getResults() public view resultsPublished returns(uint[]) {
+        return results;
+    }
 
+
+    
     
 }
